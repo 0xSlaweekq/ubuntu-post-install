a85cccc558209d4dfdc7357002c3bf7427d74d7e
Slaweekq <celesik@gmail.com>
2025-03-12 15:53:44 +0700

    new

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+#
+# sudo sed -i -e 's/kernel-open$/kernel/g' /etc/nvidia/kernel.conf
+# echo "options nvidia-drm modeset=1 fbdev=1" | sudo tee /etc/modprobe.d/nvidia-modeset.conf
+# echo "options nvidia NVreg_EnableGpuFirmware=0" | sudo tee -a /etc/modprobe.d/nvidia-modeset.conf
+# sudo chmod 644 /etc/modprobe.d/nvidia-modeset.conf
+# sudo akmods --rebuild
+# sudo dracut -f
+# reboot
 
-# // !/usr/bin/env bash #####
 
 ##Color
 RED='\033[0;31m'
